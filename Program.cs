2523790a1d2e06eda912af64248c303a8ac44a86
ikari <sydnazar08@gmail.com>
2025-04-14 20:18:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using MathCore;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace MathCore
+{
+    public interface IMathOperation
+    {
+            string OperatorSymbol { get; }
+            string Description { get; }
+            double Calculate(double a, double b);
+        }
+}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var operations = new List<IMathOperation>();
+        }
+    }
